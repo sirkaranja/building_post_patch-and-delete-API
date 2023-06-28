9c42d42e9ae025d06db3d6575b5a39a80c3b6ab1
daniel <sirkaranja12@gmail.com>
2023-06-29 00:31:52 +0300

    done with app initialization

--- app.py
+++ app.py
@@ -8,3 +8,14 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
 
+
+#Get method to retrive all books
+@app.route('/books', methods=['GET'])
+def get_books():
+    books= Book.query.all()
+    return jsonify([book.serialize()for book in books])
+
+
+
+if __name__ =='__main__':
+    app.run(port=5555)
