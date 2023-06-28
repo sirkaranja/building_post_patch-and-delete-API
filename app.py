06461c058312c9ec4420a37b2f98af39c7da1496
daniel <sirkaranja12@gmail.com>
2023-06-29 00:48:39 +0300

    app config

--- app.py
+++ app.py
@@ -1,21 +1,48 @@
 from flask import Flask, jsonify, request
 from models import db, Book
-from seed import seed_data
 
-app= Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///books.db'
 db.init_app(app)
 
-
-#Get method to retrive all books
+# GET method to retrieve all books
 @app.route('/books', methods=['GET'])
 def get_books():
-    books= Book.query.all()
-    return jsonify([book.serialize()for book in books])
+    books = Book.query.all()
+    return jsonify([book.serialize() for book in books])
 
+# POST method to add a new book
+@app.route('/books', methods=['POST'])
+def add_book():
+    title = request.json['title']
+    author = request.json['author']
+    new_book = Book(title=title, author=author)
+    db.session.add(new_book)
+    db.session.commit()
+    return jsonify(new_book.serialize()), 201
 
+# PATCH method to update an existing book
+@app.route('/books/<int:book_id>', methods=['PATCH'])
+def update_book(book_id):
+    book = Book.query.get(book_id)
+    if not book:
+        return jsonify({'error': 'Book not found'}), 404
 
-if __name__ =='__main__':
-    app.run(port=5555)
+    book.title = request.json.get('title', book.title)
+    book.author = request.json.get('author', book.author)
+    db.session.commit()
+    return jsonify(book.serialize())
+
+# DELETE method to delete a book
+@app.route('/books/<int:book_id>', methods=['DELETE'])
+def delete_book(book_id):
+    book = Book.query.get(book_id)
+    if not book:
+        return jsonify({'error': 'Book not found'}), 404
+
+    db.session.delete(book)
+    db.session.commit()
+    return jsonify({'message': 'Book deleted'})
+
+if __name__ == '__main__':
+    app.run(port= 5555)
--- seed.py
+++ seed.py
@@ -1,15 +1,20 @@
+from app import app
 from models import db, Book
 from faker import Faker
 
 fake = Faker()
 
 def seed_data(num_books):
-    db.create_all()
+    with app.app_context():
+        db.create_all()
 
-    for _ in range(num_books):
-        title = fake.catch_phrase()
-        author = fake.name()
-        book = Book(title=title, author=author)
-        db.session.add(book)
+        for _ in range(num_books):
+            title = fake.catch_phrase()
+            author = fake.name()
+            book = Book(title=title, author=author)
+            db.session.add(book)
 
-    db.session.commit()
+        db.session.commit()
+
+if __name__ == '__main__':
+    seed_data(10)
